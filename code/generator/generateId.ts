bd67e28e277342159a7f6f374be527768318fe6a
Nuh Kan <nuhhkan@gmail.com>
2023-02-25 15:54:14 +0300

    update

--- code/generator/generateId.ts
+++ code/generator/generateId.ts
@@ -4,7 +4,6 @@
  * @param {number} length - The length of the generated ID. Default is 11.
  * @returns {string} - The generated ID.
  */
-import crypto from 'crypto';
 export function generateId(length: number = 11): string {
     const maxLength = 15; // Maximum ID length
     const minLength = 6; // Minimum ID length
@@ -17,7 +16,7 @@ export function generateId(length: number = 11): string {
         length = maxLength; // If the specified length is greater than 15, set the length to 15
     }
 
-    const randomString = Array.from(crypto.getRandomValues(new Uint8Array(length)), (byte) => byte.toString(10)).join('').padStart(length, '0'); // Generate random number and convert to string
+    const randomString = Math.floor(Math.random() * length);
 
     const timestamp = (performance.now() + Date.now()).toString().replace('.', ''); // Generate timestamp
 
